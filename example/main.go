af30a80af0dfcc2d382bfab9a8cc87d6db71973e
Kevin Mesiab <kmesiab@gmail.com>
2024-10-17 02:20:43 -0700

    docs: Add .Match() func example and info in readme

--- example/main.go
+++ example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -12,32 +13,59 @@ const (
 	idRequiredPolicyExampleFile = "./example/policies/id_required_policy.json"
 )
 
+var (
+	allowedAsset = &Asset{ID: 1, Type: "asset", Finalized: true}
+	deniedAsset  = &Asset{ID: 2, Type: "asset", Finalized: false}
+	assetList    = []*Asset{allowedAsset, deniedAsset}
+)
+
 func main() {
 
-	// Load a few policies from JSON files (./policies folder)
 	policyList, err := loadPolicies()
 	if err != nil {
-
 		log.Fatalf("error loading policies: %s", err)
 	}
 
-	// Create assets to test enforcement
-	allowedAsset := &Asset{ID: 1, Type: "asset", Finalized: true}
-	deniedAsset := &Asset{ID: 2, Type: "asset", Finalized: false}
-	assetList := []*Asset{allowedAsset, deniedAsset}
+	testPolicies(policyList)
+	matchPolicies(policyList)
+
+}
+
+func matchPolicies(policyList *[]gopolicyenforcer.Policy) {
+
+	// Create a PolicyEnforcer instance with the policies
+	e := gopolicyenforcer.NewPolicyEnforcer(policyList)
+
+	fmt.Printf("---------------MATCHES-----------------\n")
+
+	// Match the policies on the assets and print results
+	for _, asset := range assetList {
+
+		matchedPolicies := e.Match(asset)
+
+		jsonString, _ := json.MarshalIndent(matchedPolicies, "", "  ")
+		fmt.Printf("Matches: %s\n", jsonString)
+	}
+
+}
+
+func testPolicies(policyList *[]gopolicyenforcer.Policy) {
 
 	// Create a PolicyEnforcer instance with the policies
 	e := gopolicyenforcer.NewPolicyEnforcer(policyList)
 
 	// Enforce the policies on the assets and print results
 	for _, asset := range assetList {
-		if e.Enforce(asset) {
-			fmt.Printf("Asset %v is allowed\n", asset)
-		} else {
-			fmt.Printf("Asset %v is not allowed\n", asset)
-		}
-	}
 
+		if e.Enforce(asset) {
+
+			fmt.Printf("Asset %d is allowed\n", asset.ID)
+		} else {
+
+			fmt.Printf("Asset %+v is not allowed\n", asset.ID)
+		}
+
+	}
 }
 
 func loadPolicies() (*[]gopolicyenforcer.Policy, error) {
